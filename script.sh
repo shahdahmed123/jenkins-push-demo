83b5bd4ff97516af86bb702a0adffda092ba6026
root <root@localhost.localdomain>
2025-10-09 21:12:55 +0300

    add bash script for jenkins test

---
+++ script.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+echo "🚀 Jenkins Job Started..."
+echo "Current Date and Time: $(date)"
+echo "Running on machine: $(hostname)"
+echo "Listing current directory contents:"
+ls -l
+echo "✅ Script finished successfully!"
+
